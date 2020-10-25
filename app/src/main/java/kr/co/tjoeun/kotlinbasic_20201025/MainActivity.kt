c783d6dddd8809f488ca72a0515b833bca3232f0
say-102 <say70007@naver.com>
2020-10-25 10:47:49 +0900

    토스트 띄워보기 예제

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.kotlinbasic_20201025
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         toastBtn.setOnClickListener {
 
-            Log.d("메인화면", "토스트 버튼 눌림")
+            Toast.makeText(this, "토스트 버튼 눌림", Toast.LENGTH_SHORT).show()
 
         }
 
