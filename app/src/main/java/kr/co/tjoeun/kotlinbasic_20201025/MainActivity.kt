8e0b7d6b1056a6984e4f80a2de67895cc0570949
say-102 <say70007@naver.com>
2020-10-25 10:45:12 +0900

    에러로그도 찍어보기 + 토스트 띄우기 버튼도

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
@@ -2,10 +2,27 @@ package kr.co.tjoeun.kotlinbasic_20201025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logBtn.setOnClickListener {
+
+            Log.d("메인화면", "로그 버튼 눌림")
+            Log.e("메인화면", "e로 로그 찍어보기")
+
+
+        }
+
+        toastBtn.setOnClickListener {
+
+            Log.d("메인화면", "토스트 버튼 눌림")
+
+        }
+
     }
 }
